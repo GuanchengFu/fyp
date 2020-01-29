26c4774e79ee2b1d4c6331c29101ff7c5cd198aa
fgc <sggfu.rick@gmail.com>
2020-01-29 11:29:39 +0000

    2020/01/29 11:28

---
+++ fyp/core/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+def index(request):
+    return HttpResponse("Later change this webpage to include the link for login and registration.")
